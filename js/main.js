bef8e9f4196bc58d9de3eaa935787a1c8f29d724
ELCRND <jwhh2a@gmail.com>
2024-07-24 16:43:35 +0300

    new file:   index.html 	new file:   js/main.js 	new file:   package.json 	new file:   public/img/about/about_bg.jpg 	new file:   public/img/about/about_icon.png 	new file:   public/img/about/about_img.png 	new file:   public/img/common/appStore_icon.png 	new file:   public/img/common/lines.png 	new file:   public/img/common/phone.png 	new file:   public/img/common/playmarket_icon.png 	new file:   public/img/home/token_bg.png 	new file:   public/img/participate/lines.png 	new file:   public/img/participate/participate_step_2_img.png 	new file:   public/img/participate/participate_step_3_bg.png 	new file:   public/img/tokenomic/tokenomic_img.png 	new file:   scss/base/_base.scss 	new file:   scss/base/_reset.scss 	new file:   scss/blocks/about.scss 	new file:   scss/blocks/buy.scss 	new file:   scss/blocks/footer.scss 	new file:   scss/blocks/header.scss 	new file:   scss/blocks/home.scss 	new file:   scss/blocks/roadmap.scss 	new file:   scss/blocks/tokenomics.scss 	new file:   scss/main.scss

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__menu");
+const code = document.querySelector(".token__code");
+const tooltip = document.querySelector(".tooltip");
+
+// открыть/закрыть меню
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menuBtn.classList.toggle("close");
+  document.body.classList.toggle("scroll-off");
+});
+
+// закрытие меню при нажатии на ссылку
+document.querySelectorAll(".menu__link").forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("visible");
+    menuBtn.classList.remove("close");
+    document.body.classList.remove("scroll-off");
+  })
+);
+
+// копирование кода в буфер обмена
+document.querySelector(".token__copy").addEventListener("click", () => {
+  navigator.clipboard.writeText(code.innerText);
+  tooltip.classList.add("visible");
+  setTimeout(() => tooltip.classList.remove("visible"), 600);
+});
